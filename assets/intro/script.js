328ac96c81274fe33fce12a8756bc731ff606d28
Christopher Hutchinson <cshutchinson@gmail.com>
2015-10-05 23:34:45 -0600

    page navigation bar working as desired

--- assets/intro/script.js
+++ assets/intro/script.js
@@ -1,8 +1,37 @@
 $(document).ready(function(){
-  $('li').on('mouseenter', function(){
-    $(this).toggleClass('highlighted');
+  // $('li').on('mouseenter', function(){
+  //   $(this).toggleClass('highlighted');
+  // });
+  // $('li').on('mouseleave', function(){
+  //   $(this).toggleClass('highlighted');
+  // });
+  var subSections = $('.contact, .family, .interests, .about');
+  $('li').on('click', '#contact', function(){
+    $('#contact, #family, #interests, #about').removeClass('highlighted');
+
+    $(this).addClass('highlighted');
+    subSections.hide();
+    $('.contact').toggle();
   });
-  $('li').on('mouseleave', function(){
-    $(this).toggleClass('highlighted');
+  $('li').on('click', '#family', function(){
+    $('#contact, #family, #interests, #about').removeClass('highlighted');
+
+    $(this).addClass('highlighted');
+    subSections.hide();
+    $('.family').toggle();
   });
+  $('li').on('click', '#interests', function(){
+    $('#contact, #family, #interests, #about').removeClass('highlighted');
+
+    $(this).addClass('highlighted');
+    subSections.hide();
+    $('.interests').toggle();
+  });
+  $('li').on('click', '#about', function(){
+    $('#contact, #family, #interests, #about').removeClass('highlighted');
+    $(this).addClass('highlighted');
+    subSections.hide();
+    $('.about').toggle();
+  });
+
 });
