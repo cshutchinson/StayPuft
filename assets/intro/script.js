0ea0ae705f242925873048cf0ca1c83d49f1e561
Christopher Hutchinson <cshutchinson@gmail.com>
2015-10-06 19:26:42 -0600

    basic website layout complete

--- assets/intro/script.js
+++ assets/intro/script.js
@@ -1,37 +1,35 @@
 $(document).ready(function(){
-  // $('li').on('mouseenter', function(){
-  //   $(this).toggleClass('highlighted');
-  // });
-  // $('li').on('mouseleave', function(){
-  //   $(this).toggleClass('highlighted');
-  // });
-  var subSections = $('.contact, .family, .interests, .about');
-  $('li').on('click', '#contact', function(){
-    $('#contact, #family, #interests, #about').removeClass('highlighted');
-
-    $(this).addClass('highlighted');
-    subSections.hide();
-    $('.contact').toggle();
+  // mouse movement interactions
+  $('li').on('mouseenter', function(){
+    $(this).toggleClass('highlighted');
   });
-  $('li').on('click', '#family', function(){
-    $('#contact, #family, #interests, #about').removeClass('highlighted');
-
-    $(this).addClass('highlighted');
-    subSections.hide();
-    $('.family').toggle();
+  $('li').on('mouseleave', function(){
+    $(this).toggleClass('highlighted');
   });
-  $('li').on('click', '#interests', function(){
-    $('#contact, #family, #interests, #about').removeClass('highlighted');
-
-    $(this).addClass('highlighted');
-    subSections.hide();
-    $('.interests').toggle();
-  });
+  // click interactions for main navbar
+  var subSections = $('.contact, .family, .interests, .about, .intro');
   $('li').on('click', '#about', function(){
     $('#contact, #family, #interests, #about').removeClass('highlighted');
     $(this).addClass('highlighted');
     subSections.hide();
     $('.about').toggle();
   });
-
+  $('li').on('click', '#family', function(){
+    $('#contact, #family, #interests, #about').removeClass('highlighted');
+    $(this).addClass('highlighted');
+    subSections.hide();
+    $('.family').fadeToggle();
+  });
+  $('li').on('click', '#interests', function(){
+    $('#contact, #family, #interests, #about').removeClass('highlighted');
+    $(this).addClass('highlighted');
+    subSections.hide();
+    $('.interests').toggle();
+  });
+  $('li').on('click', '#contact', function(){
+    $('#contact, #family, #interests, #about').removeClass('highlighted');
+    $(this).addClass('highlighted');
+    subSections.hide();
+    $('.contact').toggle();
+  });
 });
