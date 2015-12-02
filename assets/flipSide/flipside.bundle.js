b00837d7075a293cb0bfe98dd5505ebd0fcbe5a3
Christopher Hutchinson <cshutchinson@gmail.com>
2015-12-02 08:32:17 -0700

    new version of flipside

--- assets/flipSide/flipside.bundle.js
+++ assets/flipSide/flipside.bundle.js
@@ -50,8 +50,12 @@
 	var cardImages = [];
 	var promises = [];
 	var numCards = 12;
-	var delay = 500;
+	var delay = 250;
 	var openingAnimationComplete = false;
+
+
+	$('.highscorenumeric').replaceWith('<h2 class="highscorenumeric">' +
+	  (+localStorage.getItem('highScore')).toFixed(0) + '</h2>');
 
 	for (var i=0; i<numCards; i++){
 	  promises.push(hf.retrieveImage());
@@ -78,7 +82,7 @@
 
 	module.exports = {
 	  createCard: function(selector, image, num){
-	    $(selector).append('<div class="container shadow"><div class="card"><div\
+	    $(selector).append('<div class="container"><div class="card"><div\
 	     class="face front"><p>'+num+'</p></div><div class="face back"><img\
 	     id="'+ num + '" src="' + image + '"</div></div></div>');
 	  },
@@ -120,13 +124,12 @@
 	  addClickEventListener: function(target, cssClass){
 	    // usage addClickEventListener('.card', 'flip')
 	    $(target).click(function(){
-	      $(this).toggleClass(cssClass);
+	      $(this).addClass(cssClass);
 	    });
-	    return true;
 	  },
 
 	  removeClickEventListener: function(target){
-	    $(target).unbind('click');
+	    $(target).off('click');
 	  },
 
 	  elmiminateDuplicateArrayElements: function(arr){
@@ -187,24 +190,22 @@
 	  endTime: 0
 	};
 
+	mainTimer = null;
+
 	function gameLoop(){
 	  // animation is complete now - make it possible to reveal a card
+
 	  hf.addClickEventListener('.card', 'flip');
 	  $('.turnMessage>h2').replaceWith('<h2 class="turnMessage">' + 'Go! Time counts!' + '</h2>');
 	  if (score.startTime === 0) {
 	    score.startTime = Date.now();
 	  };
-	  var mainTimer = setInterval(onTimerTick, 600);
-	  function onTimerTick() {
-	    checkForMatch(mainTimer);
-	  }
+	  mainTimer = setInterval(checkForMatch, 30);
 	}
 
-	function checkForMatch(timer){
+	function checkForMatch(){
 	  // this function handles game state managment
-
-	  var $flippedCards = $('div.card.flip>div.face.back>img')
-	    .not($('div.card.matched>div.face.back>img'));
+	  var $flippedCards = $('div.card.flip').not($('div.card.matched'));
 	  // user flipped a card
 	  if ($flippedCards.length === 1){
 	    if (score.startTime===0) {
@@ -216,27 +217,22 @@
 	    if (score.endTime===0) {
 	      score.endTime = Date.now();
 	    }
-	    hf.removeClickEventListener($flippedCards);
 	    //check to see that the src of the images are equal
-	    if ($flippedCards[0].src === $flippedCards[1].src){
-	      $flippedCards.parent().parent().addClass('matched');
-	      $flippedCards.parent().parent().removeClass('flip');
-	      $('.turnMessage>h2').replaceWith('<h2 class="turnMessage">' + matchMessage(true) + '</h2>');
-
+	    if ($flippedCards.find('img')[0].src === $flippedCards.find('img')[1].src){
+	      $flippedCards.addClass('matched');
+	      $flippedCards.removeClass('flip');
 	      correctMatchScore();
 	    } else {
 	      setTimeout(function(){
-	        $flippedCards.parent().parent().removeClass('flip');
-	        $('.turnMessage>h2').replaceWith('<h2 class="turnMessage">' + matchMessage(false) +
-	          '</h2>');
-	        }, 500);
+	        $flippedCards.removeClass('flip');
+	      }, 300); // delay so user can see cards don't match
 	      incorrectMatchScore();
 	    }
 	  }
 	  // check to see if all cards are matched and end game
-	  var $matchedCards = $('div.card.matched>div.face.back>img');
+	  var $matchedCards = $('div.card.matched');
 	  if ($matchedCards.length === $('div.card').length){
-	    window.clearInterval(timer);
+	    window.clearInterval(mainTimer);
 	    gameComplete();
 	  }
 	}
@@ -251,7 +247,7 @@
 	    score.score -= turnScore;
 	    if (score.score < 0) score.score = 0;
 	  }
-	  $('.scoreNumeric').replaceWith('<h2 class="scoreNumeric game-status">' + score.score.toFixed(0) + '</h2>');
+	  $('.scoreNumeric').replaceWith('<h2 class="scoreNumeric">' + score.score.toFixed(0) + '</h2>');
 	  score.endTime = 0;
 	  score.startTime = 0;
 	  return score;
@@ -269,23 +265,6 @@
 	  calculateScore(score, true);
 	}
 
-	function matchMessage(match){
-	  matchMessages = ['You are rocking it!', 'Great match.', 'Speed demon!',
-	    'Your memory is astonishing!', 'Are you a genius?', 'Your parents must be\
-	    proud!', 'Way to go!', 'I admire your finesse!', 'Your performance is\
-	    impressive!', 'Wow, a raise is in your future!', 'Your kids will be super \
-	    intelligent!', 'Can you do that again?', 'Watch out your boss is coming!'];
-	  failMessages = ['You can do better than this!', 'Sad...try again!', 'Keep \
-	    trying!', 'Is this the best you can do?', 'Do you need another drink?',
-	    'This game can really help you.', 'Good thing you found this game!',
-	    'This is the worst performance ever!', 'Did you repeat a grade?']
-	    ;
-	  if (match){
-	    return (matchMessages[Math.floor(Math.random()*matchMessages.length)]);
-	  } else {
-	    return (failMessages[Math.floor(Math.random()*failMessages.length)]);
-	  }
-	}
 
 	function gameComplete(){
 	  // display game complete message
